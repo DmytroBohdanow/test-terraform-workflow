baf49f69f5c629d88c0284a71b401f17ee6ad069
Dmytro Bohdanov <bohdanov.dm@gmail.com>
2023-05-07 18:59:09 +0200

    fix

--- main.tf
+++ main.tf
@@ -1,7 +1,3 @@
-variable "greeting" {
-  default = "Hello, world!"
-}
-
 resource "local_file" "hello_world_function" {
   content  = "def hello_world():\n    return '${var.greeting}'\n"
   filename = "hello_world.py"
